269879791cb46227d9d6f2be60cd89f00fd81047
Belown-Laptop <washen@ethz.ch>
2025-11-13 14:03:35 +0100

    Uploads

--- src/Tools/auxiliary/__init__.py
+++ src/Tools/auxiliary/__init__.py
@@ -2,7 +2,5 @@ from .build_vector import build_vector
 from .gen_random_fixations import gen_random_fixations
 from .metadata import metadata_query
 from .parse_corrected_emip_data import parse_corrected_emip_data
-from .visualize_score import visualize_multimatch_scores
-from .visualize_score import visualize_all_scores
 from .parse_single_experiment import parse_single_experiment
 from .remove_duplicate import process_files_with_structure_verified
--- src/Tools/image_generation/__init__.py
+++ src/Tools/image_generation/__init__.py
@@ -1,3 +1,5 @@
 from .emip_gen import emip_gen
 from .aimadi_gen import aimadi_gen
 from .mcchesney_gen import mcchesney_gen
+from .visualize_score import visualize_multimatch_scores
+from .visualize_score import visualize_all_scores
